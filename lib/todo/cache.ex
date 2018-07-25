606ea82d0d5841192b1dea46bfb504c8654c57fb
Adam Pogwizd <adampog77@gmail.com>
2018-07-24 21:17:29 -0500

    Added the databse process

--- lib/todo/cache.ex
+++ lib/todo/cache.ex
@@ -12,6 +12,7 @@ defmodule Todo.Cache do
 
   #SERVER
   def init(_) do
+    Todo.Database.start()
     {:ok, %{}}
   end
 
@@ -19,7 +20,7 @@ defmodule Todo.Cache do
     case Map.fetch(todo_servers, todo_list_name) do
       {:ok, todo_server} -> {:reply, todo_server, todo_servers}
       :error ->
-        {:ok, new_server} = Todo.Server.start()
+        {:ok, new_server} = Todo.Server.start(todo_list_name)
         {
           :reply,
            new_server,

8eaddf9c7b42aa8b9eac725484ecbd8e3f911d56
Adam Pogwizd <adampog77@gmail.com>
2018-08-01 20:25:43 -0500

    Changed todo server back to genserver to timeout servers after idle period

--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -1,30 +1,58 @@
 defmodule Todo.Server do
-  use Agent, restart: :temporary
+  use GenServer, restart: :temporary
 
   @db_module Application.get_env(:todo, :db_module)
+  @expiry_idle_timeout :timer.seconds(10)
 
   def start_link(name) do
-    Agent.start_link(
-      fn ->
-        IO.puts("Starting to-do server.")
-        {name, @db_module.get(name) || Todo.List.new()}
-      end,
-      name: via_tuple(name)
-    )
+    IO.puts("Starting to-do server.")
+    GenServer.start_link(__MODULE__, name, name: via_tuple(name))
   end
 
   def add_entry(todo_server, new_entry) do
-    Agent.cast(todo_server, fn {name, todo_list} ->
-      new_list = Todo.List.add_entry(todo_list, new_entry)
-      @db_module.store(name, new_list)
-      {name, new_list}
-    end)
+    GenServer.cast(todo_server, {:add_entry, new_entry})
   end
 
   def entries(todo_server, date) do
-    Agent.get(todo_server, fn({name, todo_list}) ->
-      Todo.List.entries(todo_list, date)
-    end)
+    GenServer.call(todo_server, {:entries, date})
+  end
+
+  @impl GenServer
+  def init(name) do
+    # db read will block cache process.
+    # could send self message and init in handle_info callback
+    # works since process isnt registered, and pid won't return until after msg is sent
+    {:ok, {name, @db_module.get(name) || Todo.List.new()}, @expiry_idle_timeout}
+  end
+
+  @impl GenServer
+  def handle_cast({:add_entry, new_entry}, {name, todo_list}) do
+    new_list = Todo.List.add_entry(todo_list, new_entry)
+    @db_module.store(name, new_list)
+
+    {:noreply, {name, new_list}, @expiry_idle_timeout}
+  end
+
+  @impl GenServer
+  def handle_call({:entries, date}, _, {name, todo_list}) do
+    {
+      :reply,
+      Todo.List.entries(todo_list, date),
+      {name, todo_list},
+      @expiry_idle_timeout
+    }
+  end
+
+  @impl GenServer
+  def handle_info(:timeout, {name, todo_list}) do
+    IO.puts("Stopping to-do server for #{name}")
+    {:stop, :normal, {name, todo_list}}
+  end
+
+  @impl GenServer
+  def handle_info(unknown_message, state) do
+    super(unknown_message, state)
+    {:noreply, state, @expiry_idle_timeout}
   end
 
   defp via_tuple(name) do

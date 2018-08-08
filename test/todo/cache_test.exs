430b5dabfe2f69712292c95b4b20ad446e28f66c
Adam Pogwizd <adampog77@gmail.com>
2018-08-07 21:08:39 -0500

    turn todo into full application

--- test/todo/cache_test.exs
+++ test/todo/cache_test.exs
@@ -3,14 +3,12 @@ defmodule TodoCacheTest do
 
   describe "server_process" do
     test "caches servers" do
-      Todo.Cache.start_link(nil)
       bob_pid = Todo.Cache.server_process("bob")
 
       assert bob_pid == Todo.Cache.server_process("bob")
     end
 
     test "returns new server for different name" do
-      Todo.Cache.start_link(nil)
       bob_pid = Todo.Cache.server_process("bob")
 
       assert bob_pid != Todo.Cache.server_process("alice")

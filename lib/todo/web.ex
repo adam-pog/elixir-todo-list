e661f9e37762e98bcd6cfbc7b92a526ffff1eb6d
Adam Pogwizd <adampog77@gmail.com>
2018-08-09 20:38:07 -0500

    added entries endpoint

--- lib/todo/web.ex
+++ lib/todo/web.ex
@@ -7,8 +7,8 @@ defmodule Todo.Web do
   post "/add_entry" do
     # IO.inspect(conn)
     conn = Plug.Conn.fetch_query_params(conn)
-    list_name = Map.fetch(conn.params, "list")
-    title = Match.fetch!(conn.params, "title")
+    list_name = Map.fetch!(conn.params, "list")
+    title = Map.fetch!(conn.params, "title")
     date = Date.from_iso8601!(Map.fetch!(conn.params, "date"))
 
     list_name
@@ -18,6 +18,26 @@ defmodule Todo.Web do
     conn
     |> Plug.Conn.put_resp_content_type("text/plain")
     |> Plug.Conn.send_resp(200, "OK")
+  end
+
+  get "/entries" do
+    conn = Plug.Conn.fetch_query_params(conn)
+    list_name = Map.fetch!(conn.params, "list")
+    date = Date.from_iso8601!(Map.fetch!(conn.params, "date"))
+
+    entries =
+      list_name
+      |> Todo.Cache.server_process()
+      |> Todo.Server.entries(date)
+
+    formatted_entries =
+      entries
+      |> Enum.map(&"#{&1.date} #{&1.title}")
+      |> Enum.join("\n")
+
+    conn
+    |> Plug.Conn.put_resp_content_type("text/plain")
+    |> Plug.Conn.send_resp(200, formatted_entries)
   end
 
   def child_spec(_arg) do

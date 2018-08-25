db2c9c8f7890b3201f822f41bfa8d47b7ef5c2d6
Adam Pogwizd <adampog77@gmail.com>
2018-08-24 22:42:08 -0500

    refactor web to use parsers plug and accept any key-value pair

--- lib/todo/web.ex
+++ lib/todo/web.ex
@@ -1,38 +1,38 @@
 defmodule Todo.Web do
   use Plug.Router
+  use Plug.ErrorHandler
 
-  plug(:match)
-  plug(:dispatch)
+  plug(Plug.Parsers, parsers: [:urlencoded, :multipart])
+  plug :match
+  plug :dispatch
 
   post "/add_entry" do
-    # IO.inspect(conn)
-    conn = Plug.Conn.fetch_query_params(conn)
-    list_name = Map.fetch!(conn.params, "list")
-    title = Map.fetch!(conn.params, "title")
-    date = Date.from_iso8601!(Map.fetch!(conn.params, "date"))
+    list_name = conn.params["list"]
+    key = conn.params["key"]
+    value = conn.params["value"]
 
     list_name
     |> Todo.Cache.server_process()
-    |> Todo.Server.add_entry(%{title: title, date: date})
+    |> Todo.Server.add_entry(%{key => value})
 
     conn
     |> Plug.Conn.put_resp_content_type("text/plain")
-    |> Plug.Conn.send_resp(200, "OK")
+    |> Plug.Conn.send_resp(200, "YOU DID IT")
   end
 
   get "/entries" do
-    conn = Plug.Conn.fetch_query_params(conn)
-    list_name = Map.fetch!(conn.params, "list")
-    date = Date.from_iso8601!(Map.fetch!(conn.params, "date"))
+    list_name = conn.params["list"]
+    key = conn.params["key"]
+    value = conn.params["value"]
 
     entries =
       list_name
       |> Todo.Cache.server_process()
-      |> Todo.Server.entries(date)
+      |> Todo.Server.entries(key, value)
 
     formatted_entries =
       entries
-      |> Enum.map(&"#{&1.date} #{&1.title}")
+      |> Enum.map(&"#{&1[key]} #{&1[value]}")
       |> Enum.join("\n")
 
     conn
@@ -46,5 +46,9 @@ defmodule Todo.Web do
       options: [port: Application.fetch_env!(:todo, :http_port)],
       plug: __MODULE__
     )
+  end
+
+  def handle_errors(conn, %{kind: _kind, reason: reason, stack: _stack}) do
+    IO.puts("Something went wrong")
   end
 end

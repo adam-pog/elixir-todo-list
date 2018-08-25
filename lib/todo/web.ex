5f4ed2339cbca77d93667758e2d51e24b0796662
Adam Pogwizd <adampog77@gmail.com>
2018-08-25 10:20:40 -0500

    added basic request logging. Accept arbitrary json list got adding entry

--- lib/todo/web.ex
+++ lib/todo/web.ex
@@ -1,19 +1,21 @@
+require IEx
+
 defmodule Todo.Web do
   use Plug.Router
   use Plug.ErrorHandler
 
-  plug(Plug.Parsers, parsers: [:urlencoded, :multipart])
+  plug Plug.Logger
+  plug(Plug.Parsers, parsers: [:urlencoded, :multipart, :json], json_decoder: Poison)
   plug :match
   plug :dispatch
 
   post "/add_entry" do
-    list_name = conn.params["list"]
-    key = conn.params["key"]
-    value = conn.params["value"]
+    list_name = conn.params["list_name"]
+    list = conn.params["list"]
 
     list_name
     |> Todo.Cache.server_process()
-    |> Todo.Server.add_entry(%{key => value})
+    |> Todo.Server.add_entry(list)
 
     conn
     |> Plug.Conn.put_resp_content_type("text/plain")
@@ -36,7 +38,7 @@ defmodule Todo.Web do
       |> Enum.join("\n")
 
     conn
-    |> Plug.Conn.put_resp_content_type("text/plain")
+    |> Plug.Conn.put_resp_content_type("text/plain") #return / accept JSON?
     |> Plug.Conn.send_resp(200, formatted_entries)
   end
 
@@ -49,6 +51,7 @@ defmodule Todo.Web do
   end
 
   def handle_errors(conn, %{kind: _kind, reason: reason, stack: _stack}) do
-    IO.puts("Something went wrong")
+    IO.inspect("Something went wrong:")
+    IO.inspect reason
   end
 end

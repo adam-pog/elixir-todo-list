988458e311aec5123088f5e537d59dad6b3e3e1d
Adam Pogwizd <adampog77@gmail.com>
2018-09-03 14:50:27 -0500

    added test for GET /entries

--- test/todo_web/controllers/todo_controller_test.exs
+++ test/todo_web/controllers/todo_controller_test.exs
@@ -16,7 +16,25 @@ defmodule TodoWeb.TodoControllerTest do
         where: entry.name == ^list_name,
         select: entry.list
       )
-      assert res = %{success: true}
+      assert %{"success" => true} = res
       assert ^saved_list = list
+  end
+
+  describe "GET /entries" do
+    test "key and value are given", %{conn: conn} do
+      list_name = "get_entries_name"
+      key = "key"
+      value = "value"
+      list = %{key => value}
+
+      Todo.Repo.insert!(%Todo.TodoList{name: list_name, list: list})
+
+      res =
+        conn
+        |> get(todo_path(conn, :entries, list_name: list_name, key: key, value: value))
+        |> json_response(200)
+
+      assert ^res = [list]
+    end
   end
 end

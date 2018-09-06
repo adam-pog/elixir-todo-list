9c8638aa6a478c2d60164db1b63ed6520ad41cbb
Adam Pogwizd <adampog77@gmail.com>
2018-09-06 13:02:13 -0500

    added tests for todo server

--- test/todo/cache_test.exs
+++ test/todo/cache_test.exs
@@ -2,7 +2,7 @@ defmodule TodoCacheTest do
   use Todo.DataCase
 
   test "server_process" do
-    cache = Todo.Cache.start_link()
+    Todo.Cache.start_link()
     server1_pid = Todo.Cache.server_process("server_through_cache")
 
     assert Todo.Cache.server_process("server_through_cache") == server1_pid
--- test/todo/server_test.exs
+++ test/todo/server_test.exs
@@ -1,11 +1,40 @@
 defmodule TodoServerTest do
-  # use ExUnit.Case
-  # use Todo.DataCase
-  #  test "add_entry" do
-  #   {:ok, pid} = Todo.Server.start_link("name")
-  #   date = ~D[2018-12-12]
-  #   Todo.Server.add_entry(pid, %{date:  date, title: "title"})
-  #   entries = Todo.Server.entries(pid, :date, date)
-  #    assert [%{date: ^date, title: "title"}] = entries
-  # end
+  use Todo.DataCase
+  import Ecto.Query, only: [from: 2]
+
+  test "add_entry" do
+    new_entry = %{"key" => "value"}
+    list_name = "name"
+    result = Todo.Server.handle_call({:add_entry, new_entry}, nil, {list_name, []})
+    [saved_list] = Todo.Repo.all(
+      from entry in Todo.TodoList,
+      where: entry.name == ^list_name,
+      select: entry.list
+    )
+
+    assert ^result = {:reply, :success, {list_name, [new_entry]}, :timer.seconds(30)}
+    assert ^saved_list = new_entry
+  end
+
+  test "entries/3" do
+    key = "key"
+    value = "value"
+    list = %{key => value}
+    list2 = %{"key2" => "value2"}
+    list_name = "name"
+
+    result = Todo.Server.handle_call({:entries, key, value}, nil, {list_name, [list, list2]})
+
+    assert ^result = {:reply, [list], {list_name, [list, list2]}, :timer.seconds(30)}
+  end
+
+  test "entries/1" do
+    list = %{"key" => "value"}
+    list2 = %{"key2" => "value2"}
+    list_name = "name"
+
+    result = Todo.Server.handle_call({:entries}, nil, {list_name, [list, list2]})
+
+    assert ^result = {:reply, [list, list2], {list_name, [list, list2]}, :timer.seconds(30)}
+  end
 end
